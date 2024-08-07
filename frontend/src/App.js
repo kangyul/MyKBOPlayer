b34907dd70741f6b76364214408e8678e05362c4
Yul Kang <39001942+kangyul@users.noreply.github.com>
2024-08-07 14:21:08 +0000

    add kbo team logos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
+import exampleLogo from './assets/images/1001.svg'
+
 function App() {
   return (
     <div>
       <p>MY KBO PLAYER</p>
+      <img src={exampleLogo} alt="" />
     </div>
   );
 }
