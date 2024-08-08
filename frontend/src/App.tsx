1a689d2e189f6d0b5ae187879adc6a960bc7596e
Yul Kang <39001942+kangyul@users.noreply.github.com>
2024-08-08 14:16:45 +0000

    build: install vite, tailwindcss

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="flex justify-around">
+      <div className="flex flex-row">
+        <div className="flex flex-col">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+        <div>2</div>
+      </div>
+    </div>
+    
+  )
+}
+
+export default App
