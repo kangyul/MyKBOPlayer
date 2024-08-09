a197422d9b5a774e994c7faf24a7e1c3c4f543c9
Yul Kang <39001942+kangyul@users.noreply.github.com>
2024-08-09 11:40:19 +0000

    style: page structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,20 @@
 function App() {
   return (
-    <div className="flex justify-around">
-      <div className="flex flex-row">
-        <div className="flex flex-col">
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
+    <>
+      <div className="flex justify-center items-center flex-col">
+        <div className="content-center">
+          <h1 className="m-4 font-extrabold text-gray-700 text-4xl">My KBO Player</h1>
         </div>
-        <div>2</div>
+        <div className="flex flex-row">
+          <div className="flex flex-col basis-1/2 h-90">
+            <div className="h-90 bg-slate-100 p-5 m-5 rounded-md">1</div>
+            <div className="h-90 bg-slate-100 p-5 m-5 rounded-md">1</div>
+            <div className="h-90 bg-slate-100 p-5 m-5 rounded-md">1</div>
+          </div>
+          <div className="basis-1/2 bg-slate-100 p-5 m-5 rounded-md">2</div>
+        </div>
       </div>
-    </div>
-    
+    </>
   )
 }
 
